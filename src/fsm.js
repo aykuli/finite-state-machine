877d1150e79388d46f3f0c0a4e8c50fed7566203
Aykuli <aykuli@ya.ru>
2019-10-11 00:37:38 +0700

    feat: add task

--- src/fsm.js
+++ src/fsm.js
@@ -6,10 +6,10 @@ class FSM {
     constructor(config) {
         this.config = config;
         this.state = config.initial;
-        this.stackOfStates = [config.initial];
+        this.stackOfStates = [ ['normal', null, null, 1 ] ];//pattern [data, prev, next, length]
         this.getStatesArr = [];
         this.undoState = config.initial;
-        this.undoCount = 0;
+        this.undoIndex = 0;
 
         if (config == null) {
             throw new Error('config isn\'t passed');
@@ -32,11 +32,14 @@ class FSM {
         if (this.stackOfStates[this.stackOfStates.length - 1] == 'Error' ) {            
             throw new Error('Error in stackOfStates');
         } else if (state in this.config.states && state !== this.state) {
-            this.state = state;
+            
             this.undoState = this.state;
-            this.stackOfStates.push(state);
+            
+            this.stackOfStates.push([state, this.state, null, this.stackOfStates.length + 1]);
+            this.state = state;
+            this.stackOfStates[this.stackOfStates.length - 2][2] = this.state; // next value for previous value
         } else {
-            this.stackOfStates.push('Error');
+            this.stackOfStates.push(['Error', this.state, null, this.stackOfStates.length + 1]);
             throw new Error('hmmm... exception?');
         }
         return this.state;
@@ -47,22 +50,36 @@ class FSM {
      * @param event
      */
     trigger(event) {
-        if (this.stackOfStates[this.stackOfStates.length - 1] == 'Error') {
+        if (this.stackOfStates[this.stackOfStates.length - 1][0] == 'Error') {
             throw new Error('event in current state isn\'t exist');
         }
         
         for (let key in this.config.states) {
             for (let transitionKey in this.config.states[key].transitions )  {
                 if (event == transitionKey) {
-                    this.stackOfStates.push(this.config.states[key].transitions[event]);
+                    for (let i = 0; i < this.undoIndex; i++) {
+                        this.stackOfStates.pop();
+                    }
+                    this.undoIndex = 0;
+
+                    this.stackOfStates.push( [  this.config.states[key].transitions[event], 
+                                                this.state,
+                                                null,
+                                                this.stackOfStates.length + 1
+                    ]);
+                    
+                    if (this.stackOfStates > 1) {
+                        this.stackOfStates[this.stackOfStates.length - 2][2] = this.state;
+                    }
                     this.state = this.config.states[key].transitions[event];
+                    
                     this.undoState = this.state;
                     return this.state;
                 }
             } 
         }
         
-        throw new Error('event in current state isn\'t exist');   
+        throw new Error('event in current state isn\'t exist');    
     }
 
     /**
@@ -70,8 +87,8 @@ class FSM {
      */
     reset() {
         this.state = this.config.initial;
-        this.stackOfStates = [];
-        this.undoState = this.state;
+        this.stackOfStates = [ ['normal', null, null, 1 ] ];//pattern [data, prev, next, length]
+        // this.undoState = this.state;
     }
 
     /**
@@ -106,14 +123,18 @@ class FSM {
      * @returns {Boolean}
      */
     undo() {
-        if (this.undoState === this.config.initial) {
+        if (this.stackOfStates[this.stackOfStates.length - 1][3] === 1) {
+            return false;
+        }
+
+        if (this.undoIndex === this.stackOfStates.length - 1) {
             return false;
         }
     
-        this.state = this.stackOfStates[this.stackOfStates.length - this.undoCount - 2];
-        this.undoState = this.state;
-        this.undoCount++;
-
+        
+        this.state = this.stackOfStates[this.stackOfStates.length - this.undoIndex - 2][0];
+        this.undoIndex++;
+        
         return true;
     }
 
@@ -123,14 +144,15 @@ class FSM {
      * @returns {Boolean}
      */
     redo() {
-        if (this.undoState === this.config.initial && this.undoState.length == 0) {
-            return false;
-        }
-        this.state = this.stackOfStates[this.stackOfStates.length - this.undoCount];
-        this.undoState = this.state;        
-        this.undoCount--;
-                
-        return true;
+        // if (this.undoState === this.config.initial && this.stackOfStates.length == 1) {
+        //     return false;
+        // }
+
+        // this.state = this.stackOfStates[this.stackOfStates.length - this.undoCount];
+        // this.undoState = this.state;
+        // this.undoCount--;
+        
+        // return true;
     }
 
     /**
--- src/sandbox.js
+++ src/sandbox.js
@@ -34,81 +34,73 @@ class FSM {
     constructor(config) {
         this.config = config;
         this.state = config.initial;
-        this.stackOfStates = [config.initial];
+        this.stackOfStates = [ ['normal', null, null, 1 ] ];//pattern [data, prev, next, length]
         this.getStatesArr = [];
         this.undoState = config.initial;
-        this.undoCount = 0;
+        this.undoIndex = 0;
+        // this.prev = null;
 
         if (config == null) {
             throw new Error('config isn\'t passed');
         }
     }
-
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
     getState() {
         return this.state;
     }
-
-    /**
-     * Goes to specified state.
-     * @param state
-     */
     changeState(state) {
         if (this.stackOfStates[this.stackOfStates.length - 1] == 'Error' ) {            
             throw new Error('Error in stackOfStates');
         } else if (state in this.config.states && state !== this.state) {
+            this.stackOfStates.push([state, this.state, null, this.stackOfStates.length + 1]);
             this.state = state;
-            this.undoState = this.state;
-            this.stackOfStates.push(state);
+            this.stackOfStates[this.stackOfStates.length - 2][2] = this.state; // next value for previous value
         } else {
-            this.stackOfStates.push('Error');
+            this.stackOfStates.push(['Error', this.state, null, this.stackOfStates.length + 1]);
             throw new Error('hmmm... exception?');
         }
         return this.state;
     }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
     trigger(event) {
-        console.log('------------------   trigger  -----------------');
-        if (this.stackOfStates[this.stackOfStates.length - 1] == 'Error') {
+        console.log(`\n------------------   trigger(${event})  -----------------`);
+        if (this.stackOfStates[this.stackOfStates.length - 1][0] == 'Error') {
             throw new Error('event in current state isn\'t exist');
         }
+        console.log('WAS this.stackOfStates =', this.stackOfStates);
         
         for (let key in this.config.states) {
             for (let transitionKey in this.config.states[key].transitions )  {
                 if (event == transitionKey) {
-                    this.stackOfStates.push(this.config.states[key].transitions[event]);
+                    for (let i = 0; i < this.undoIndex; i++) {
+                        this.stackOfStates.pop();
+                    }
+                    this.undoIndex = 0;
+
+                    this.stackOfStates.push( [  this.config.states[key].transitions[event], 
+                                                this.state,
+                                                null,
+                                                this.stackOfStates.length + 1
+                    ]);
+
+                    
                     this.state = this.config.states[key].transitions[event];
-                    this.undoState = this.state;
+                    if (this.stackOfStates > 1) {
+                        this.stackOfStates[this.stackOfStates.length - 2][2] = this.state;
+                    }
+                    
+        console.log('BECOME this.stackOfStates =', this.stackOfStates);
+        console.log('undoIndex =', this.undoIndex);
                     return this.state;
                 }
             } 
         }
-        
         throw new Error('event in current state isn\'t exist');   
     }
-
-    /**
-     * Resets FSM state to initial.
-     */
     reset() {
         this.state = this.config.initial;
-        this.stackOfStates = [];
-        this.undoState = this.state;
+        this.stackOfStates = [ ['normal', null, null, 1 ] ];//pattern [data, prev, next, length]
     }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
     getStates(event) {
         this.getStatesArr = [];
 
@@ -128,72 +120,44 @@ class FSM {
         }
         return this.getStatesArr;
     }
-
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
     undo() {
         console.log('\n------------------   undo  -----------------');
-        if (this.undoState === this.config.initial) {
+        if (this.stackOfStates[this.stackOfStates.length - 1][3] === 1) {
             return false;
         }
-    
-        this.state = this.stackOfStates[this.stackOfStates.length - this.undoCount - 2];
-        console.log('this.state = ', this.state);
-        this.undoState = this.state;
-        console.log('this.undoState = ', this.undoState);
-        this.undoCount++;
+        if (this.undoIndex === this.stackOfStates.length - 1) {
+            return false;
+        }
+        console.log('WAS this.state = ', this.state);
+        this.state = this.stackOfStates[this.stackOfStates.length - this.undoIndex - 2][0];
+        
+        this.undoIndex++;
+        console.log('BECOME this.state = ', this.state);
+        console.log('undoIndex = ', this.undoIndex);
         console.log('this.stackOfStates = ', this.stackOfStates);
-
+        // this.state = this.stackOfStates[this.stackOfStates.length - 1][1];
         return true;
     }
-
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
     redo() {
         console.log('\n------------------   redo  -----------------');
-        if (this.undoState === this.config.initial && this.undoState.length == 1) {
+        if (this.undoState === this.config.initial && this.stackOfStates.length == 1) {
             return false;
         }
-        this.state = this.stackOfStates[this.stackOfStates.length - this.undoCount];
-        console.log('this.state = ', this.state);
-        this.undoState = this.state;
-        
-        console.log('this.undoState = ', this.undoState);
-        this.undoCount--;
-        
-        console.log('this.stackOfStates = ', this.stackOfStates);
         
         return true;
     }
-
-    /**
-     * Clears transition history
-     */
     clearHistory() {}
 }
 
 console.log('\n-------------------------------------------------------------------------');
-
 const student = new FSM(config);
+
 student.trigger('study');
 console.log('student.getState() =', student.getState()); //.to.equal('busy');
 student.undo();
-student.redo();
+console.log('student.getState() =', student.getState()); //.to.equal('normal');
+
+student.trigger('study');
+student.trigger('get_hungry');
+student.undo();
 console.log('student.getState() =', student.getState()); //.to.equal('busy');
-
-// student.trigger('get_tired');
-// student.trigger('get_hungry');
-
-// student.undo();
-// student.undo();
-
-// student.redo();
-// student.redo();
-
-// console.log('student.getState() =', student.getState()); //.to.equal('hungry');
